f9ded4b355b59384c3516dfd0a7c46aa94293752
globalz159 <soutomv@hotmail.com>
2024-11-27 10:04:42 -0300

    alterações na porta

--- src/server.js
+++ src/server.js
@@ -6,17 +6,20 @@ const session = require('./config/session');
 
 const server = express();
 
+// Configuração da sessão
 server.use(session);
 server.use((req, res, next) => {
   res.locals.session = req.session;
   next();
 });
 
+// Middleware
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
 server.use(methodOverride('_method'));
 server.use(routes);
 
+// Configuração do Nunjucks
 server.set('view engine', 'njk');
 nunjucks.configure('src/app/views', {
   express: server,
@@ -24,6 +27,7 @@ nunjucks.configure('src/app/views', {
   noCache: process.env.NODE_ENV !== 'production',
 });
 
+// Porta dinâmica para Vercel
 const PORT = process.env.PORT || 8082;
 server.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}.`);
