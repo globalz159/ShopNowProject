068d758c5b4e7fc0f0b1ecba1abdb09e59ccc825
globalz159 <soutomv@hotmail.com>
2024-11-27 09:57:34 -0300

    adjust server configs to vercel

--- src/server.js
+++ src/server.js
@@ -18,13 +18,13 @@ server.use(methodOverride('_method'));
 server.use(routes);
 
 server.set('view engine', 'njk');
-
 nunjucks.configure('src/app/views', {
   express: server,
-  autoescape: false,
-  noCache: true
+  autoescape: true,
+  noCache: process.env.NODE_ENV !== 'production',
 });
 
-server.listen(8082, function () {
-  console.log('server is running on port 8082.');
+const PORT = process.env.PORT || 8082;
+server.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}.`);
 });
